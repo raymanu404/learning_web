63142b5c22f50f0ee9f78007db95730b41655479
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2024-10-14 23:19:01 +0300

    add custom controller to example 3 of video

--- js/index.js
+++ js/index.js
@@ -7,3 +7,33 @@ const changeUlType = () => {
 }
 
 changeUlType()
+
+const playButton = document.querySelector("#play-button ")
+playButton.classList.add("display-block")
+playButton.addEventListener("click", playHandler, false)
+
+const pauseButton = document.querySelector("#pause-button ")
+pauseButton.classList.add("display-none")
+pauseButton.addEventListener("click", stopHandler, false)
+
+function stopHandler() {
+  const media = document.getElementById("video-controller")
+  media.pause()
+  pauseButton.classList.remove("display-block")
+  pauseButton.classList.add("display-none")
+  playButton.classList.add("display-block")
+
+  document.querySelector("[aria-controls]").removeAttribute("controls")
+}
+
+function playHandler() {
+  const media = document.getElementById("video-controller")
+  if (media.paused) {
+    media.play()
+    playButton.classList.remove("display-block")
+    playButton.classList.add("display-none")
+    pauseButton.classList.add("display-block")
+  }
+
+  document.querySelector("[aria-controls]").removeAttribute("controls")
+}
