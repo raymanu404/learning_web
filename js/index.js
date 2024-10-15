03853710ce342c48fadeb5fef6bcf482327dcc9d
Emanuel Caprariu <emanuel.caprariu@molsondev.com>
2024-10-15 18:18:18 +0300

    prepare template, slot and shadow section

--- js/index.js
+++ js/index.js
@@ -37,3 +37,17 @@ function playHandler() {
 
   document.querySelector("[aria-controls]").removeAttribute("controls")
 }
+
+//TO RENDER TEMPLATE EXAMPLE 1
+const containerToRender = document.querySelector('#render-template-rating-stars')
+const ratingTemplate = document.getElementById('star-rating-template')
+const ratingTemplateContent = ratingTemplate.content
+containerToRender.appendChild(ratingTemplateContent)
+
+
+//TO RENDER TEMPLATE EXAMPLE 2
+const containerToRender2 = document.querySelector('#slot-example-container')
+const ratingTemplate2 = document.getElementById('slot-template-example-template')
+const ratingTemplateContent2 = ratingTemplate2.content
+containerToRender2.appendChild(ratingTemplateContent2)
+
