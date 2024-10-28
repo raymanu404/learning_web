591dc24ee444765e17796483f519af2f59c6cf29
Emanuel Caprariu <emanuel.caprariu@molsondev.com>
2024-10-28 19:09:37 +0200

    complete validation section from form learning

--- dist/ts/form-validation.js
+++ dist/ts/form-validation.js
@@ -5,4 +5,35 @@ likeButton === null || likeButton === void 0 ? void 0 : likeButton.addEventListe
     likeButtonCount++;
     alert(`You like this form for ${likeButtonCount}`);
 });
+const usernameInput = document.querySelector('[name="username-2"]');
+usernameInput === null || usernameInput === void 0 ? void 0 : usernameInput.addEventListener("invalid", () => {
+    usernameInput.setCustomValidity("Please introduce  At least 2 chars, maximum 20, lowercase, uppercase, digits only");
+});
+const formInput3 = document.getElementById("username-3");
+const errorMessage = document.getElementById("username3-validation");
+formInput3 === null || formInput3 === void 0 ? void 0 : formInput3.addEventListener("blur", (event) => {
+    const lowerLetters = new RegExp("[a-z]");
+    const upperLetters = new RegExp("[A-Z]");
+    const digits = new RegExp("[0-9]");
+    const value = formInput3.value;
+    let message = "";
+    if (!lowerLetters.test(value) &&
+        !upperLetters.test(value) &&
+        !digits.test(value)) {
+        message += "Your input must contain lower, upper letters or digits\n";
+    }
+    else {
+        message = "";
+    }
+    if (value.length < 2 || value.length > 20) {
+        message += "Your input must has a length between 2 and 20 characters\n";
+    }
+    else {
+        message = "";
+    }
+    errorMessage.innerText = message;
+}, true);
+formInput3 === null || formInput3 === void 0 ? void 0 : formInput3.addEventListener("focus", (ev) => {
+    errorMessage.innerText = "";
+});
 //# sourceMappingURL=form-validation.js.map
--- src/ts/form-validation.ts
+++ src/ts/form-validation.ts
@@ -1,6 +1,57 @@
-const likeButton = document.getElementById("like-button")
-let likeButtonCount = 0
+const likeButton = document.getElementById("like-button");
+let likeButtonCount = 0;
 likeButton?.addEventListener("click", () => {
-  likeButtonCount++
-  alert(`You like this form for ${likeButtonCount}`)
-})
+  likeButtonCount++;
+  alert(`You like this form for ${likeButtonCount}`);
+});
+
+//custom validity message
+const usernameInput = document.querySelector(
+  '[name="username-2"]'
+) as HTMLInputElement;
+
+usernameInput?.addEventListener("invalid", () => {
+  usernameInput.setCustomValidity(
+    "Please introduce  At least 2 chars, maximum 20, lowercase, uppercase, digits only"
+  );
+});
+
+const formInput3 = document.getElementById("username-3") as HTMLInputElement;
+const errorMessage = document.getElementById(
+  "username3-validation"
+) as HTMLSpanElement;
+
+formInput3?.addEventListener(
+  "blur",
+  (event) => {
+    // Validate the field
+    const lowerLetters = new RegExp("[a-z]");
+    const upperLetters = new RegExp("[A-Z]");
+    const digits = new RegExp("[0-9]");
+
+    const value = formInput3.value;
+    let message = "";
+    if (
+      !lowerLetters.test(value) &&
+      !upperLetters.test(value) &&
+      !digits.test(value)
+    ) {
+      message += "Your input must contain lower, upper letters or digits\n";
+    } else {
+      message = "";
+    }
+
+    if (value.length < 2 || value.length > 20) {
+      message += "Your input must has a length between 2 and 20 characters\n";
+    } else {
+      message = "";
+    }
+
+    errorMessage.innerText = message;
+  },
+  true
+);
+
+formInput3?.addEventListener("focus", (ev) => {
+  errorMessage.innerText = "";
+});
