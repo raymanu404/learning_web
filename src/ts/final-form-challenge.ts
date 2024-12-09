62a2f7dbf9ee0715d021fef7565e76b8bbf4fa5b
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2024-12-10 00:06:01 +0200

    initialize final form challenge

--- src/ts/final-form-challenge.ts
+++ src/ts/final-form-challenge.ts
@@ -1,2 +1,28 @@
 // 🟦#03.CHALLENGE: CREATE A COMPLEX FORM WITH ALL FIELDS (NAME, EMAIL, PASSWORD, ADDRESS FORM, PAYMENT FORM, FILE INPUT, RADIO BUTTONS, CHECKBOXES ETC)
 console.log('final challenge from form section');
+
+const openDialogButton = document.getElementById(
+  'final_challenge_button',
+) as HTMLButtonElement;
+const closeDialogButton = document.getElementById(
+  'close_button',
+) as HTMLButtonElement;
+const dialog = document.getElementById(
+  'final_form_dialog',
+) as HTMLDialogElement;
+const backdrop = document.getElementById('backdrop');
+const side_nav = document.getElementById('side-nav-id');
+
+openDialogButton?.addEventListener('click', () => {
+  dialog.open = true;
+
+  backdrop?.classList.remove('hidden');
+  side_nav?.classList.add('hidden');
+});
+
+closeDialogButton?.addEventListener('click', () => {
+  dialog.open = false;
+
+  side_nav?.classList.remove('hidden');
+  backdrop?.classList.add('hidden');
+});
