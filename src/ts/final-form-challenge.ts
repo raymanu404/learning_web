1a09c5ddf6e687b446fe01d5153fe8a5bc9187ad
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2025-01-04 02:21:15 +0200

    refactor, restructure, enable route query params to open dialog

--- src/ts/final-form-challenge.ts
+++ src/ts/final-form-challenge.ts
@@ -1,5 +1,15 @@
 // 🟦#03.CHALLENGE: CREATE A COMPLEX FORM WITH ALL FIELDS (NAME, EMAIL, PASSWORD, ADDRESS FORM, PAYMENT FORM, FILE INPUT, RADIO BUTTONS, CHECKBOXES ETC)
-console.log('final challenge from form section');
+
+//#################################################
+//TYPES
+//#################################################
+enum ParamQueryEnum {
+  DIALOG = 'dialog',
+}
+
+//#################################################
+//HTML ELEMENTS
+//#################################################
 
 const openDialogButton = document.getElementById(
   'final_challenge_button',
@@ -13,16 +23,63 @@ const dialog = document.getElementById(
 const backdrop = document.getElementById('backdrop');
 const side_nav = document.getElementById('side-nav-id');
 
-openDialogButton?.addEventListener('click', () => {
+//#################################################
+//FUNCTIONS
+//#################################################
+
+const parseStringToBoolean = (str: string) => {
+  return str.toLowerCase() === 'true';
+};
+
+const getRouteQueryParams = (searchValue: string): string[] => {
+  const { search } = window.location;
+  const searchParsed = search.replace('?', '');
+  const searchedSplit = searchParsed.split('&');
+  const dialogParamPairKey = searchedSplit
+    .map((x) => {
+      const [key, value] = x.split('=');
+      if (key.includes(searchValue)) {
+        return [key, value];
+      }
+    })
+    .flat()
+    .filter((x) => x !== undefined);
+
+  return dialogParamPairKey;
+};
+
+const openDialogFn = () => {
   dialog.open = true;
 
   backdrop?.classList.remove('hidden');
   side_nav?.classList.add('hidden');
-});
+};
 
-closeDialogButton?.addEventListener('click', () => {
+const closeDialogFn = () => {
   dialog.open = false;
 
   side_nav?.classList.remove('hidden');
   backdrop?.classList.add('hidden');
+};
+
+//#################################################
+//EVENTS
+//#################################################
+
+window.addEventListener('DOMContentLoaded', () => {
+  const [keyParam, valueParam] = getRouteQueryParams(ParamQueryEnum.DIALOG);
+
+  if (
+    valueParam &&
+    parseStringToBoolean(valueParam) &&
+    keyParam === ParamQueryEnum.DIALOG
+  ) {
+    openDialogFn();
+  }
 });
+
+openDialogButton?.addEventListener('click', () => {
+  openDialogFn();
+});
+
+closeDialogButton?.addEventListener('click', () => {});
